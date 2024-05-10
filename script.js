582f58fb296fab47be088bbacfe62f0816bb2d9f
Vlad7292domadoma <133425890+Vlad7292domadoma@users.noreply.github.com>
2024-05-10 13:28:23 +0300

    desw

--- script.js
+++ script.js
@@ -565,7 +565,7 @@ const btnUp = {
             // определяем величину прокрутки
             const scrollY = window.scrollY || document.documentElement.scrollTop;
             // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
-            scrollY > 100 ? this.show() : this.hide();
+            scrollY > 500 ? this.show() : this.hide();
         });
         // при нажатии на кнопку .btn-up
         document.querySelector('.btn-up').onclick = () => {
